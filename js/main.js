8e951459f42892cc418ee3a9f3dca8db8528405d
Jason Serafica <seraficaj@gmail.com>
2022-10-14 14:43:08 -0700

    bring in pseudocode

---
+++ js/main.js
@@ -0,0 +1,36 @@
+/*----- constants -----*/
+const types = ["Fire", "Water", "Grass"];
+
+/*----- state variables -----*/
+//  state = {
+//   p1Choice: choice,
+//   p2Choice: choice,
+//   score: Number,
+//  }
+/*----- cached elements  -----*/
+//   fireButton
+//   waterButton
+//   grassButton
+//   p1Score
+//  p2Score
+// winnerDisplay
+// resetButton
+
+/*----- event listeners -----*/
+//   handleChoice
+//   handleReset
+
+/*----- functions -----*/
+//   compareChoices
+//       - update state values:
+//           - update score
+//           - replace player's choices to none because they have to pick for next round
+
+//   declareWinner
+//       - check score after max rounds
+//       - return winner
+
+//   render
+//       - show player's choices
+//       - show score
+//       - show winner/loser
